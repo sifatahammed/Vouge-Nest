030ad5f012e2b6e807a5153fa66456ae057b75a7
sifatahammed <sifatahammed821@gamail.com>
2025-09-03 19:09:07 +0600

    Add BestSeller Section into HomePage

--- project-frontend/pages/Home.jsx
+++ project-frontend/pages/Home.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Hero from '../components/Hero'
 import LatestCollection from '../components/LatestCollection'
+import BestSeller from '../../../New folder/forever-main/frontend/src/components/BestSeller'
 
 const Home = () => {
   return (
     <div>
       <Hero/>
       <LatestCollection/>
-
+      <BestSeller/>
     </div>
   )
 }
