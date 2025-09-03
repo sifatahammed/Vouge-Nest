59ed9284dfdbbca9c216b568160b1994d60fb471
sifatahammed <sifatahammed821@gamail.com>
2025-09-03 15:58:04 +0600

    Add Hero Section of HomePage

---
+++ project-frontend/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Hero from '../components/Hero'
+
+const Home = () => {
+  return (
+    <div>
+      <Hero/>
+      <LatestCollection/>
+
+    </div>
+  )
+}
+
+export default Home;
