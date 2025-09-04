4231d55538ca726135b55c59f39b4e078a42cd72
sifatahammed <sifatahammed821@gamail.com>
2025-09-04 18:59:48 +0600

    Add Order Page

---
+++ project-frontend/src/pages/Orders.jsx
@@ -0,0 +1,46 @@
+import React, { useContext, useEffect, useState } from 'react'
+import { ShopContext } from '../context/ShopContext';
+import Title from '../components/Title';
+
+const Orders = () => {
+  const {currency} = useContext(ShopContext);
+
+  const [orderData, setOrderData] = useState([]);
+
+  
+  return (
+    <div className='border-t pt-16'>
+      <div className='text-2xl'>
+        <Title text1 = {'MY'} text2 = {'ORDERS'}/>
+      </div>
+      <div>
+        {orderData.slice(1,4).map((item, index)=>(
+          <div key={index} className='py-4 border-b text-gray-700 flex flex-col md:flex-row md:items-center md:justify-between gap-4'>
+            <div className='flex items-start gap-6 text-sm'>
+              <img className='w-16 sm:w-20' src={item.images[0]} alt="" />
+              <div>
+              <p className='sm:text-base font-medium'>{item.name}</p>
+              <div className='flex items-center gap-3 mt-1 text-base text-gray-700'>
+                <p>{currency}{item.price}</p>
+                <p>Quantity: {item.quantity}</p>
+                <p>Size: {item.size}</p>
+              </div>
+              <p className='mt-1'>Date: <span className='text-gray-400'>{new Date(item.date).toDateString()}</span></p>
+              <p className='mt-1'>Payment: <span className='text-gray-400'>{item.paymentMethod}</span></p>
+              </div>
+            </div>
+            <div className='md:w-1/2 flex justify-between'>
+              <div className='flex items-center gap-2'>
+                <p className='min-w-2 -2 rounded-full bg-green-500'></p>
+                <p className='text-sm md:text-base'>{item.status}</p>
+              </div>
+              <button onClick={loadOrderData} className='border px-4 py-2 text-sm font-medium rounded-sm'>Track Order</button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default Orders;
