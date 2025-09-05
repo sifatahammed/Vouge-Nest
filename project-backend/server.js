0c9d4cdf3365f8953cef59854e9749561bc21678
sifatahammed <sifatahammed821@gamail.com>
2025-09-05 19:19:42 +0600

    Connect User and Product Endpoints

--- project-backend/server.js
+++ project-backend/server.js
@@ -3,6 +3,8 @@ import cors from 'cors'
 import 'dotenv/config' 
 import connectDB from './config/mongodb.js'
 import connectCloudinary from './config/cloudinary.js'
+import userRouter from './routes/userRoute.js'
+import productRouter from './routes/productRoute.js'
 
 //App config
 const app = express()
@@ -13,6 +15,9 @@ connectCloudinary();
 // middlewares
 app.use(express.json())
 app.use(cors())
+
+app.use('/api/user', userRouter);
+app.use('/api/product', productRouter);
 
 app.get('/', (req, res) => {
     res.send("API working")
