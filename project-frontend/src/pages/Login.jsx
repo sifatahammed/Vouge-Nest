01661097885be09be6964c8d9ba660a899093b54
sifatahammed <sifatahammed821@gamail.com>
2025-09-06 15:19:14 +0600

    Connect LogIn SignIn BackEnd

--- project-frontend/src/pages/Login.jsx
+++ project-frontend/src/pages/Login.jsx
@@ -1,13 +1,50 @@
 import React, { useState } from 'react'
+import { useContext } from 'react';
+import { ShopContext } from '../context/ShopContext';
+import axios from 'axios';
+import { useEffect } from 'react';
+import { toast } from 'react-toastify';
 
 const Login = () => {
   const [currentState, setCurrentState]= useState('Sign Up');
+  const {token, setToken, navigate, backendUrl} = useContext(ShopContext);
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const onSubmitHandler = async (event) => {
     event.preventDefault();
+    try{
+      if(currentState === 'Sign Up'){
+        const response = await axios.post(backendUrl + '/api/user/register', {name, email, password})
+        if(response.data.success){
+          setToken(response.data.token)
+          localStorage.setItem('token', response.data.token)
+        }
+        else{
+          toast.error(response.data.message)
+        }
+      }
+      else{
+        const response = await axios.post(backendUrl + 'api/user/login', {email, password})
+        if(response.data.success){
+          setToken(response.data.token)
+          localStorage.setItem('token', response.data.token)
+        }
+        else{
+          toast.error(response.data.message)
+        }
+      }
+    }
+    catch (error){
+      console.log(error);
+      toast.error(error.message)
+    }
   }
+  useEffect(()=>{
+    if(token){
+      navigate('/')
+    }
+  })
   return (
     <div>
       <form onSubmit={onSubmitHandler} className='flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800'>
