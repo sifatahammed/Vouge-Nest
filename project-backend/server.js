dcb11c4452ea3df90be6dd5edbccd99117f5efd4
sifatahammed <sifatahammed821@gamail.com>
2025-09-07 15:53:32 +0600

    Add Order EndPoints to BackEnd

--- project-backend/server.js
+++ project-backend/server.js
@@ -6,21 +6,22 @@ import connectCloudinary from './config/cloudinary.js'
 import userRouter from './routes/userRoute.js'
 import productRouter from './routes/productRoute.js'
 import cartRouter from './routes/cartRoute.js'
+import orderRouter from './routes/orderRoute.js'
 
 //App config
 const app = express()
 const port = process.env.PORT || 4000
 connectDB();
 connectCloudinary();
-
 // middlewares
 app.use(express.json())
 app.use(cors())
 
+// api endpoints
 app.use('/api/user', userRouter);
 app.use('/api/product', productRouter);
 app.use('/api/cart', cartRouter);
-
+app.use('/api/order', orderRouter);
 app.get('/', (req, res) => {
     res.send("API working")
 })
