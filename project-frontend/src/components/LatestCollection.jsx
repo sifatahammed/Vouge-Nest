3c3a1f0c6e4d5aed0291204d9f9c639a4c4b7a11
MD Sifat Ahammed Akash <116369555+sifatahammed@users.noreply.github.com>
2026-09-25 20:10:14 +0600

    Update LatestCollection.jsx

--- project-frontend/src/components/LatestCollection.jsx
+++ project-frontend/src/components/LatestCollection.jsx
@@ -16,7 +16,7 @@ const LatestCollection = () => {
       <div className='text-center py-8 text-3xl'>
         <Title text1={'LATEST'} text2={'COLLECTIONS'} />
         <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta ad pariatur dolores ea voluptas placeat nulla eum repudiandae deserunt libero.
+         Discover the newest trends in fashion, footwear and accessories-designed for modern lifestyles, crafted with premium quality and timeless style.
         </p>
       </div>
       {/* Rendering products */}
